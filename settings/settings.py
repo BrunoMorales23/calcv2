f49be4d41763f0221baf5ca80d1d94bffadd7c6a
Bruno Morales <brunomorales0323@outlook.es>
2025-08-19 17:45:21 -0300

    Removed Cachés

--- settings/settings.py
+++ settings/settings.py
@@ -6,8 +6,9 @@ dotenv_path = join(dirname(__file__), '.env')
 load_dotenv()
 
 def init():
-    global popplerPath, inputPath, tesseractPath, logsPath
+    global popplerPath, inputPath, tesseractPath, logsPath, promptBase
     tesseractPath = os.environ.get("TESSERACT_EXE")
     popplerPath = os.environ.get("POPPLER_PATH")
     inputPath = os.environ.get("INPUT_PATH")
     logsPath = os.environ.get("LOGS_PATH")
+    promptBase = os.environ.get("PROMPTBASE_PATH")
