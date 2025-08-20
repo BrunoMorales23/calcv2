ca48b120d553f3ee86f16ea5fba5ac1264d2885d
Bruno Morales <brunomorales0323@outlook.es>
2025-08-19 21:16:45 -0300

    Llama & Main Fixes | Requirements Added

--- main.py
+++ main.py
@@ -4,6 +4,8 @@ from settings import *
 from queue import *
 from log import *
 from llama import *
+from gemini import *
+import re
 
 settings.init()
 work_queue = queue.Queue()
@@ -25,9 +27,25 @@ for file in files:
 while work_queue.size() != 0:
     current_item = work_queue.peek()
     item_content = current_item.content
+    item_path = current_item.path
     
+    #LLama
+    #------------------------------------------------------------
     llama_template = ollama.getTemplate(settings.promptBase)
     llama_result = ollama.executePrompt(str(llama_template), item_content)
-    #print(llama_result)
+    print(llama_result)
     logs.writeLogValue(current_log, llama_result)
+    #------------------------------------------------------------
+
+    #Gemini
+    #------------------------------------------------------------
+    #gemini_content = gemini.executePrompt(pdf_path=item_path, prompt="Detecta los siguientes puntos y responde de la forma más breve posible, ya sea respondiento por 'Si' o 'No', o bien, declarando la cantidad (en números) de elementos que cumplen con la condición:" \
+    #"- ¿Es necesario hacer reporte de ejecución?" \
+    #"- ¿Cuántas aplicaciones/aplicativos/programas son requeridos en la automatización?" \
+    #"- ")
+
+    #regex_flag = re.search("^¿*?$", gemini_content)
+    #if regex_flag == True:
+    #    regex_content = re.sub( "(?<=\[)(.*?)(?=\])", "", gemini_content)
+    #    print(regex_content)
     break
--- settings/settings.py
+++ settings/settings.py
@@ -6,9 +6,10 @@ dotenv_path = join(dirname(__file__), '.env')
 load_dotenv()
 
 def init():
-    global popplerPath, inputPath, tesseractPath, logsPath, promptBase
+    global popplerPath, inputPath, tesseractPath, logsPath, promptBase, gemini_api_key
     tesseractPath = os.environ.get("TESSERACT_EXE")
     popplerPath = os.environ.get("POPPLER_PATH")
     inputPath = os.environ.get("INPUT_PATH")
     logsPath = os.environ.get("LOGS_PATH")
     promptBase = os.environ.get("PROMPTBASE_PATH")
+    gemini_api_key = os.environ.get("GEMINI_API_KEY")
