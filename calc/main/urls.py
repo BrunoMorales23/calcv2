e61943070007af377a2df71b0fe0ed0cc29ff346
Bruno Morales <brunomorales0323@outlook.es>
2025-08-26 19:49:35 -0300

    prg improvement in views file

--- calc/main/urls.py
+++ calc/main/urls.py
@@ -1,8 +1,14 @@
 from django.urls import path
 from . import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path("", views.home, name="home"),
     path("test/", views.test, name="test"),
-    path("uploaded/", views.upload_file, name="upload")
+    path("uploaded/", views.upload_file, name="upload"),
+    path("loading/", views.loading, name="loading")
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- calc/main/views.py
+++ calc/main/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
+from django.core.files.storage import FileSystemStorage
+from django.urls import reverse
 
 # Create your views here.
 def home(request):
@@ -9,6 +11,21 @@ def test(request):
     return render(request, "test.html")
 
 def upload_file(request):
-    if request.method == "POST":
-        saludo = "AAAAAAAAAAAAAAAAAAAAAAAAAAA"
-        return render(request, "test.html", {"filecontent":saludo})
+    if request.method == "GET":
+        filename = request.GET.get("filename")
+        return render(request, "test.html", {"filename": filename})
+    elif request.method == "POST":
+        action = request.POST.get("action")
+        if action == "iniciar":
+            return redirect(reverse("loading"))
+        archivo = request.FILES.get("archivo")
+        if archivo:
+            fs = FileSystemStorage()
+            filename = fs.save(archivo.name, archivo)
+            return redirect(reverse("upload") + f"?filename={filename}")
+        else:
+            return redirect(reverse("home"))
+
+def loading(request):
+    if request.method == "GET":
+        return render(request, "loading.html")
